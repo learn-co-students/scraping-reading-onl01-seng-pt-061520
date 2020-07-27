82ac1e84dc00915a85822562b121f6fde70ed346
idalispaz <idalispaz@gmail.com>
2020-07-26 20:30:38 -0700

    Done.

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -2,3 +2,18 @@ require 'nokogiri'
 require 'open-uri'
 
 html = open("https://flatironschool.com/")
+
+doc = Nokogiri::HTML(open("https://flatironschool.com/"))
+puts doc.css(".headline-260IBN").text
+
+doc = Nokogiri::HTML(html)
+
+doc.css(".inlineMobileLeft-2Yo002.imageTextBlockGrid3-2XAK6G")
+
+courses = doc.css("#2a778efd-1685-5ec6-9e5a-0843d6a88b7b .inlineMobileLeft-2Yo002.imageTextBlockGrid3-2XAK6G")
+
+course.each do |course|
+    puts course.text.strip
+end 
+
+p doc.css("#2a778efd-1685-5ec6-9e5a-0843d6a88b7b .inlineMobileLeft-2Yo002.imageTextBlockGrid3-2XAK6G")[0]
